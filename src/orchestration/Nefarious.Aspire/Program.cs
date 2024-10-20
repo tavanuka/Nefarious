da7a1b7f74581316f65ea448c521042639ab735e
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2024-10-20 03:03:21 +0200

    feat(redis): add data persistence through volumes

--- src/orchestration/Nefarious.Aspire/Program.cs
+++ src/orchestration/Nefarious.Aspire/Program.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("nefarious-cache");
+var cache = builder
+    .AddRedis("nefarious-cache")
+    .WithDataVolume();
 
 builder.AddProject<Projects.Nefarious_Host>("nefarious-host")
     .WithReference(cache);
