c2a83cbb356fc7a85d4468962164b7fef30e5517
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-01-05 01:15:07 +0100

    refactor: move core service registration to a central core extension file

---
+++ src/Nefarious.Core/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nefarious.Common.Options;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using Serilog;
+
+namespace Nefarious.Core.Extensions;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers Options bindings from appsettings.json configuration file.
+    /// </summary>
+    /// <param name="services">The service collection context to use for registration.</param>
+    /// <param name="configuration">Configuration context of the appsettings.json.</param>
+    /// <returns>Updated service collection context.</returns>
+    public static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<DiscordOptions>()
+            .Bind(configuration.GetRequiredSection(DiscordOptions.SectionName))
+            .ValidateOnStart();
+        services.AddOptions<SpotifyOptions>()
+            .Bind(configuration.GetRequiredSection(SpotifyOptions.SectionName))
+            .ValidateOnStart();
+
+        return services;
+    }
+
+    public static IServiceCollection AddLogging(this IServiceCollection services, IConfiguration configuration)
+    {
+        var otlpExporter = configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+        var serviceName = configuration["OTEL_SERVICE_NAME"] ?? "Undefined";
+        
+        services.AddSerilog((sp, loggerConfiguration) => {
+            loggerConfiguration
+                .ReadFrom.Configuration(configuration)
+                .ReadFrom.Services(sp)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+
+            // This will get skipped if the env variable does not exist, making it optional.
+            // In the future, it could be also conditionally turned on using a bool flag.
+            if (!string.IsNullOrEmpty(otlpExporter))
+                loggerConfiguration
+                    .WriteTo.OpenTelemetry(options => {
+                        options.Endpoint = otlpExporter;
+                        options.ResourceAttributes.Add("service.name", serviceName);
+                    });
+        });
+        
+        return services;
+    }
+
+    public static IServiceCollection AddOpenTelemetry(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+    {
+        var otlpExporter = configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+
+        services.AddOpenTelemetry()
+            .WithMetrics(metrics => metrics
+                .AddRuntimeInstrumentation()
+                .AddHttpClientInstrumentation())
+            .WithTracing(tracing => {
+                if (environment.IsDevelopment())
+                    tracing.SetSampler(new AlwaysOnSampler());
+            });
+
+        // Configure telemetry providers if OTLP exporter exists.
+        if (string.IsNullOrWhiteSpace(otlpExporter))
+            return services;
+        {
+            services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+            services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
+            services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
+        }
+
+        return services;
+    }
+}
--- src/Nefarious.Host/Program.cs
+++ src/Nefarious.Host/Program.cs
@@ -1,12 +1,8 @@
-using Nefarious.Common.Options;
 using Nefarious.Core.Extensions;
 using Nefarious.Core.Services;
 using Nefarious.Spotify.Extensions;
 using Nefarious.Spotify.Publishers;
 using Nefarious.Spotify.Services;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Trace;
 using Serilog;
 
 // Two-stage initialization: https://github.com/serilog/serilog-aspnetcore?tab=readme-ov-file#two-stage-initialization
@@ -18,55 +14,19 @@ Log.Logger = new LoggerConfiguration()
 try
 {
     var builder = Host.CreateApplicationBuilder(args);
-    var config = builder.Configuration;
-
-    var otlpExporter = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-    var serviceName = builder.Configuration["OTEL_SERVICE_NAME"] ?? "Undefined";
-
-    builder.Services.AddOptions<DiscordOptions>()
-        .Bind(config.GetRequiredSection(DiscordOptions.SectionName))
-        .ValidateOnStart();
-    builder.Services.AddOptions<SpotifyOptions>()
-        .Bind(config.GetRequiredSection(SpotifyOptions.SectionName))
-        .ValidateOnStart();
-
-    builder.Services.AddSerilog((sp, loggerConfiguration) => {
-        loggerConfiguration
-            .ReadFrom.Configuration(builder.Configuration)
-            .ReadFrom.Services(sp)
-            .Enrich.FromLogContext()
-            .WriteTo.Console();
-
-        if (!string.IsNullOrEmpty(otlpExporter))
-            loggerConfiguration
-                .WriteTo.OpenTelemetry(options => {
-                    options.Endpoint = otlpExporter;
-                    options.ResourceAttributes.Add("service.name", serviceName);
-                });
-    });
-
-    builder.Services.AddOpenTelemetry()
-        .WithMetrics(metrics => metrics
-            .AddRuntimeInstrumentation()
-            .AddHttpClientInstrumentation())
-        .WithTracing(tracing => {
-            if (builder.Environment.IsDevelopment())
-                tracing.SetSampler(new AlwaysOnSampler());
-        });
-
-    // Add OTLP Exporters.
-    if (!string.IsNullOrWhiteSpace(otlpExporter))
-    {
-        builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
-        builder.Services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
-        builder.Services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
-    }
+    var configuration = builder.Configuration;
+    var environment = builder.Environment;
+    
+    builder.Services
+        .AddOptions(configuration)
+        .AddLogging(configuration)
+        .AddOpenTelemetry(configuration, environment);
 
     builder.AddRedisClient("nefarious-cache");
     builder.AddRedisDistributedCache(connectionName: "nefarious-cache");
     
     builder.Services.AddSpotifyClient();
-    builder.Services.AddDiscordWebsocketClient(config);
+    builder.Services.AddDiscordWebsocketClient(configuration);
     
     builder.Services.AddSingleton<IPlaylistService, PlaylistService>();
     
