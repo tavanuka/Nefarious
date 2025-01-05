b39467c6c3e3f5168c749c7b9df84dc7c912acef
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-01-05 04:52:46 +0100

    feat: add initial boilerplate project files

---
+++ src/Nefarious.Api/Program.cs
@@ -0,0 +1,45 @@
+﻿// Two-stage initialization: https://github.com/serilog/serilog-aspnetcore?tab=readme-ov-file#two-stage-initialization
+using Nefarious.Core.Extensions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting application...");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    var configuration = builder.Configuration;
+    var environment = builder.Environment;
+    
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    
+    builder.Services
+        .AddOptions(configuration)
+        .AddLogging(configuration)
+        .AddOpenTelemetry(configuration, environment);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
