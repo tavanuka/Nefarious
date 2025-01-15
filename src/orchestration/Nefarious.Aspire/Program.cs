e831e0d701049cea5840b4da04f45d9491ad03a6
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-01-15 15:00:09 +0100

    build: programatically determine which host to run in the orchestration

--- src/orchestration/Nefarious.Aspire/Program.cs
+++ src/orchestration/Nefarious.Aspire/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder
     .AddRedis("nefarious-cache")
     .WithDataVolume();
 
-builder.AddProject<Projects.Nefarious_Host>("nefarious-host")
-    .WithReference(cache);
+if(builder.Configuration.GetValue<bool>("Host"))
+    builder.AddProject<Projects.Nefarious_Api>("nefarious-api")
+        .WithReference(cache);
+else 
+    builder.AddProject<Projects.Nefarious_Host>("nefarious-host")
+        .WithReference(cache);
 
 builder.Build().Run();
