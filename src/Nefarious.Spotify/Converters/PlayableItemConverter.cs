d625ddce21ce52d2c4b2c2ce9a99d76cf2adc1d1
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-01-29 00:12:48 +0100

    feat(json): implement custom converter to handle enums
    This commit adds the ability to actually use the better json (de)serializer because spotify nuget library is still using the fucking newtonsoft one? what is this, 2015? This converter also ensures backwards compatibility in case a numerical value slipped through the json formatting and not designated a string representation of the specified enums.
    Each day I stray further from god, one spaghetti line at the time.

---
+++ src/Nefarious.Spotify/Converters/PlayableItemConverter.cs
@@ -0,0 +1,34 @@
+﻿using SpotifyAPI.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nefarious.Spotify.Converters;
+
+public class SystemPlayableItemConverter : JsonConverter<IPlayableItem>
+{
+    public override IPlayableItem? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using var doc = JsonDocument.ParseValue(ref reader);
+        if (!doc.RootElement.TryGetProperty("type", out var typeElement))
+            throw new JsonException("Unable to determine the type of IPlayableItem.");
+
+        // ReSharper disable once SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
+        // Extremely niche case where I only need string or a number
+        var type = typeElement.ValueKind switch
+        {
+            JsonValueKind.String => typeElement.GetString(),
+            JsonValueKind.Number => Enum.GetName(typeof(ItemType), typeElement.GetInt32()),
+            _ => throw new JsonException("Unable to determine the type of IPlayableItem")
+        };
+
+        return type switch
+        {
+            nameof(ItemType.Track) => doc.RootElement.Deserialize<FullTrack>(options),
+            nameof(ItemType.Episode) => doc.RootElement.Deserialize<FullEpisode>(options),
+            _ => throw new NotSupportedException($"Unsupported item type: {type}")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, IPlayableItem value, JsonSerializerOptions options)
+        => JsonSerializer.Serialize(writer, value, value.GetType(), options);
+}
