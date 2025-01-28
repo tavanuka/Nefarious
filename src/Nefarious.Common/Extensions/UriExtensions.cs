fd706f27104a4ae22c9d25761993e40c47f209c7
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-01-29 00:13:42 +0100

    feat(common): add Uri helper extension methods

---
+++ src/Nefarious.Common/Extensions/UriExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace Nefarious.Common.Extensions;
+
+public static class UriExtensions
+{
+    public static Uri AddParameters(this Uri url, IDictionary<string, string> parameters)
+    {
+        var uriBuilder = new UriBuilder(url);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        
+        foreach (var key in parameters.Keys)
+            query[key] = parameters[key];
+        
+        uriBuilder.Query = query.ToString();
+        return uriBuilder.Uri;
+    }
+}
