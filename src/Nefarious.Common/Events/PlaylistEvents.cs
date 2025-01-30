9e5cdd2caff37ff72b94aaa38b8ef7fe8e5f2dea
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2025-01-30 13:00:10 +0100

    feat(events): add new playlist event record types

---
+++ src/Nefarious.Common/Events/PlaylistEvents.cs
@@ -0,0 +1,7 @@
+﻿namespace Nefarious.Common.Events;
+
+public record TrackAddedToPlaylist(string PlaylistId, List<ulong> Subscribers, string PlaylistName, string UpdatedBy, TrackDetails UpdatedTrack)
+    : PlaylistUpdated(PlaylistId, Subscribers, PlaylistName, UpdatedBy, UpdatedTrack);
+
+public record TrackRemovedFromPlaylist(string PlaylistId, List<ulong> Subscribers, string PlaylistName, string UpdatedBy, TrackDetails UpdatedTrack)
+    : PlaylistUpdated(PlaylistId, Subscribers, PlaylistName, UpdatedBy, UpdatedTrack);
